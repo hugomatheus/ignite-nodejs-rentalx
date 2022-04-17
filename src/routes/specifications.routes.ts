e57a04945047ebc6640d39c1cbf228ce7777d40e
Hugo Targino <hg.targino@gmail.com>
2022-04-17 08:45:01 -0300

    refactoring create specification (useCase, Controller)

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,13 @@
 import { Request, Response, Router } from "express";
 
 import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 const specificationRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request: Request, response: Response) => {
